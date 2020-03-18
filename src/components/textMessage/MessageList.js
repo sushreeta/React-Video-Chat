df4c072a224e6ae2f0821d86e3038d7ac2c4bb2a
Sushreeta Sahoo <sushreetas@geekyants.com>
2020-03-18 19:34:07 +0530

    chore: message

--- src/components/textMessage/MessageList.js
+++ src/components/textMessage/MessageList.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles'
+import React, { useState, useEffect, useRef } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
 // let [message, handleMessage] = useState([]);
 // const addLi = (msg, name) => {
@@ -7,28 +7,51 @@ import { makeStyles } from '@material-ui/core/styles'
 //     handleMessage([...message, {name, message: msg}])
 //     console.log("pushed msg ", name, msg, message)
 //   };
-  
+
 const useStyles = makeStyles(theme => ({
-    list: {
+  list: {
     // listStyleType: none,
-    padding: '10px',
-   }}));
+    padding: "10px"
+  }
+}));
 
-const MessageList = (props)=>{
-
-    const classes = useStyles();
-    let [message, handleMessage] = useState([]);
-    handleMessage([...message, {name: props.textMessage.name, message: props.textMessage.message}])
-    console.log("pushed msg ", props)
-    return (
-        <ul id="list" className={classes.list}>
-        {message.map(item => (
-          <li key={item.message}>
-            {item.name+": "+item.message}
-            {console.log("item", item)}
-          </li>
-        ))}
-      </ul>
-    )
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
 }
+
+const MessageList = props => {
+  const classes = useStyles();
+  const [message, handleMessage] = useState([]);
+  let propMessage = props.textMessage.message;
+  const prevValue = !!usePrevious({ propMessage });
+  
+  console.log("Previous value",prevValue)
+
+  useEffect(() => {
+    
+    console.log("Previous value",prevValue)
+    if (!!prevValue.message != !!props.textMessage.message) {
+      handleMessage([
+        ...message,
+        { name: props.textMessage.name, message: props.textMessage.message }
+      ]);
+      console.log("pushed msg ", props);
+    }
+  }, [props.textMessage.message]);
+
+  return (
+    <ul id="list" className={classes.list}>
+      {message.map(item => (
+        <li key={item.message}>
+          {item.name + ": " + item.message}
+          {console.log("item", item)}
+        </li>
+      ))}
+    </ul>
+  );
+};
 export default MessageList;
