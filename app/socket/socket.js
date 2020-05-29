f3e67f4a65cbdff7f20cf9c37352a30d09e8deca
Sushreeta Sahoo <sushreetas@geekyants.com>
2020-05-29 15:09:34 +0530

    feat:video call

--- app/socket/socket.js
+++ app/socket/socket.js
@@ -2,7 +2,7 @@ const express = require("express");
 const config = require('../config/config');
 
 const app = express();
-const server = app.listen(config.SERVER_PORT);
+const server = app.listen(config.SERVER_PORT,()=>console.log('listening at',config.SERVER_PORT));
 const io = require("socket.io")(server);
 
 module.exports =  io;
